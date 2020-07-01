3a6893b6caeaab6e4b6a7151dd4a6f028c65c26e
A. Rodriguez <arguez@users.noreply.github.com>
2020-07-01 08:59:40 -0600

    Updated main files

--- source/Devices/Devices.ino
+++ source/Devices/Devices.ino
@@ -1,15 +1,43 @@
 #include "display.h"
+<<<<<<< Updated upstream
+#include "project.h"
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("\nSystem Started\n");
+=======
+<<<<<<< Updated upstream
+=======
+#include "payment.h"
+#include "project.h"
+>>>>>>> Stashed changes
 
 void setup() {
   Serial.begin(9600);
   
+<<<<<<< Updated upstream
   while(!Serial.available()) {
     delay(1000);
   }
   runTests();
+=======
+  if (testmode) {
+    runTests();
+  }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
 
-
 void loop() {
+<<<<<<< Updated upstream
+  appBegin();
+=======
+<<<<<<< Updated upstream
   
+=======
+  if (!testmode) {
+    appBegin();
+  }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
