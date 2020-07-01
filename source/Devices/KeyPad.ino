2545876cc9f812d8202f35a74c163cc3ba17b799
A. Rodriguez <arguez@users.noreply.github.com>
2020-07-01 09:16:25 -0600

    Updated keypad

--- source/Devices/KeyPad.ino
+++ source/Devices/KeyPad.ino
@@ -1,37 +1,34 @@
-/* @file CustomKeypad.pde
-|| @version 1.0
-|| @author Alexander Brevig
-|| @contact alexanderbrevig@gmail.com
-||
-|| @description
-|| | Demonstrates changing the keypad size and key values.
-|| #
-*/
 #include <Keypad.h>
 
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //four columns
-//define the cymbols on the buttons of the keypads
+const byte ROWS = 4;
+const byte COLS = 3;
+
 char hexaKeys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
-  {'*','D','#'}
-};
-byte rowPins[ROWS] = {8, 9, 10, 11}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
+  {'*','0','#'}
 
-//initialize an instance of class NewKeypad
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+// for ESP32
+byte colPins[COLS] = {14, 12, 13}; // connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {21, 19, 18, 5}; // connect to the row pinouts of the keypad
+
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
+void scanKeypad() {
+  while (appstate != READY) {
+    keyinput = customKeypad.getKey();
 
-//Serial.begin(9600);
-
-  
-void getKeyPressed(){
-  char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    Serial.println(customKey);
+    if (keyinput) {
+      //Serial.println(keyinput);
+      appstate = READY;
+    }
+    delay(100); // debounce
   }
 }
