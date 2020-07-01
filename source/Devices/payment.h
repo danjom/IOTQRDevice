b76ecd442e6e291f82ab344e02a4fb5dcd7ad6dc
A. Rodriguez <arguez@users.noreply.github.com>
2020-07-01 09:20:06 -0600

    Payment branch

---
+++ source/Devices/payment.h
@@ -0,0 +1,9 @@
+enum states {READY,   //  key has been pressed
+            SETUP,    //  first configuration
+            MENU,     //  main menu options
+            KEYPAD,   //  waiting for input
+            PURCHASE, //  value entry page
+            REDEEM,   //  redeem entry page
+            HISTORY,  //  view last transactions
+            PROCESS   //  QR display and verification
+            };
