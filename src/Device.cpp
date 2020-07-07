63751a568e0ae5cb123c2289aa5c4e264a6f6785
A. Rodriguez <arguez@users.noreply.github.com>
2020-07-07 02:50:17 -0600

    Added keypad test

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,58 +1,9 @@
 #include <Arduino.h>
-#include <KeyScan.h>
-#include <Digits.h>
-
-KeyScan scanner = KeyScan();
-Digits digits = Digits();
-
-char input;
-void check();
 
 void setup() {
-    Serial.begin(9600);
-    scanner = KeyScan();
-    Serial.println("\nProgram Started\n");
+    //Serial.println("\nProgram Started\n");
 }
 
 void loop() {
-    input = scanner.getKey();
 
-    if (input) {
-        Serial.print("Input is: ");
-        Serial.println(input);
-        check();
-    }
-}
-
-void check() {
-    if (input == '*') {
-        if (digits.isEmpty()) {
-            Serial.println("Switching previous screen");
-        }
-        else {
-            digits.trimValue();
-        }
-    }
-    else if (input == '#') {
-        if (digits.isEmpty()) {
-            Serial.println("[LED] Digits field is empty");
-        }
-        else if (!digits.isValid()) {  
-            Serial.println("[LED] Digits field is less than 1");
-            digits.clearValue();
-        }
-        else {
-            if (digits.isDecimal()) {
-                Serial.println("Processing payment for USD " + String(digits.getValue()));
-            }
-            else {
-                digits.addDecimal();
-            }
-        }
-    }
-    else {
-        digits.addNumber(input);
-    }
-    Serial.print("Field is: ");
-    Serial.println(digits.getField());
 }
